c3aa5efa4c84196d199becad925c138a58061a79
Guilherme Eiti <guilheremiua@gmail.com>
2021-07-21 15:04:47 -0300

    fix: fixing ssl

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -14,6 +14,9 @@ module.exports = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
   pool: {
     min: 2,
